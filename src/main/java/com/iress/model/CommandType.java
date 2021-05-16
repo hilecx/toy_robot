248cc37594189ff061023d71e34221b28e19b519
Chris Xu <hilecx@gmail.com>
2021-05-16 10:12:55 +1000

    change PLACE pattern to allow multi blank

--- src/main/java/com/iress/model/CommandType.java
+++ src/main/java/com/iress/model/CommandType.java
@@ -5,6 +5,6 @@ public class CommandType {
     public static final String LEFT = "LEFT";
     public static final String RIGHT = "RIGHT";
     public static final String REPORT = "REPORT";
-    public static final String PLACE = "^(PLACE)\\s\\d+,\\d+,(NORTH|WEST|EAST|SOUTH)$";
+    public static final String PLACE = "^(PLACE)\\s+\\d+,\\d+,(NORTH|WEST|EAST|SOUTH)$";
 
 }
